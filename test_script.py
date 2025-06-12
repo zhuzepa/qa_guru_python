efe264f94c6a5c23b121a413a6c2ab4a307a1b5b
Prince Aleksey <princeaa@aqa-engineer.ru>
2025-06-13 01:07:05 +0300

    del test file

--- test_script.py
+++
@@ -1,6 +0,0 @@
-from selene import browser, be, have
-
-
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('html').should(have.text('About this page'))
